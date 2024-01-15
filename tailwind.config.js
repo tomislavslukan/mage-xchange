3c9609315e7cc517c6adc26abffa3e6d154063d0
tomislavslukan <tomislav.slukan@inchoo.net>
2024-01-15 14:17:15 +0100

    tailwind config, header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,41 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'orange': '#FFBC00',
+        'turquoise': '#09A8D6',
+        'dark-gray': '#333',
+        'light-gray': '#4B545C',
+      },
+      fontFamily:{
+        amaranth: ['Amaranth'],
+        inter: ['Inter'],
+        raleway: ['Raleway'],
+      },
+      fontWeight:{
+        'regular': '400',
+        'medium': '500',
+        'bold': '700',
+      },
+      fontSize: {
+        'h1': '2.986rem',
+        'h2': '2.488rem',
+        'h3': '2.074rem',
+        'p': '1rem',
+        'small': '0.833rem',
+      },
+      spacing: {
+        '6': '0.375rem',
+        '12': '0.75rem',
+        '24': '1.5rem',
+        '30': '1.875rem',
+        '48': '1.875rem',
+      },
+      backgroundImage: {
+        'hero': "url('../static/images/header-bg.svg')",
+      }
+    },
   },
   variants: {
     extend: {},
